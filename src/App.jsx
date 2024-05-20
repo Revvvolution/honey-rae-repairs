06fc51dac856bbbf06e5935e766a0341c8e2cac8
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-05-20 15:43:29 -0400

    Created first react component using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+  const [count, setCount] = useState(0) // [stateVariable, setterFunction]  **You always want to use const when defining a useState
+
+  const handleBtnClick = () => {
+    setCount(count + 1)  // Use react dev tools to watch the change of state
+  }
+
+      // <></> is called a 'Fragment' and groups elements without a wrapper node without effecting the resulting DOM
+  return (
+        <>
+          <h1>Hello!</h1>
+          <div>This is amazing!</div>
+          <button className="btn-secondary" onClick={handleBtnClick}>
+            Click me!
+          </button>
+          <div>Count: {count}</div>
+        </>
+  )
 }
+
